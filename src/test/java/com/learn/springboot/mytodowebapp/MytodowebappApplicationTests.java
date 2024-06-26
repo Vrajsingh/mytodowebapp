2feb57c6a113ea2a6216cdfdf76a4774869a4ddb
Vishal Raj <vishalraj@Viveks-MacBook-Pro.local>
2024-06-26 16:16:50 +0530

    Initial commit

---
+++ src/test/java/com/learn/springboot/mytodowebapp/MytodowebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.mytodowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytodowebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
