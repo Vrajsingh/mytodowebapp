412e6782976d687ceb70ca2a62e25fb49b53737b
Vishal Raj <vishalraj@Viveks-MacBook-Pro.local>
2024-06-30 13:48:42 +0530

    Added logger

--- src/main/java/com/learn/springboot/mytodowebapp/login/LoginController.java
+++ src/main/java/com/learn/springboot/mytodowebapp/login/LoginController.java
@@ -1,15 +1,18 @@
 package com.learn.springboot.mytodowebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
 @Controller
 public class LoginController {
     
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     // @RequestMapping("login")
     // @ResponseBody
     // public String sayLogin() {
@@ -21,6 +24,10 @@ public class LoginController {
     @RequestMapping("login")
     public String goToLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name", name);
+
+        logger.debug("Request param is {}", name);
+        logger.info("I want this to be printed in info level.");
+        logger.warn("I want this to be printed in warn level.");
         return "login";
     }
     
