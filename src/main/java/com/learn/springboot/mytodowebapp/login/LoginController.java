0500cf26a4d979806aa884e2542a4039c91de242
Vishal Raj <vishalraj@Viveks-MacBook-Pro.local>
2024-07-01 21:38:13 +0530

    Started creating login page, created form, method post type

--- src/main/java/com/learn/springboot/mytodowebapp/login/LoginController.java
+++ src/main/java/com/learn/springboot/mytodowebapp/login/LoginController.java
@@ -1,33 +1,15 @@
 package com.learn.springboot.mytodowebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
-    
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
-    // @RequestMapping("login")
-    // @ResponseBody
-    // public String sayLogin() {
-    //     return "login";
-    // }
-
-    // Whenever we want to pass any parameters from controller to jsp we use MODELs
 
     @RequestMapping("login")
-    public String goToLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
+    public String goToLoginPage() {
 
-        logger.debug("Request param is {}", name);
-        logger.info("I want this to be printed in info level.");
-        logger.warn("I want this to be printed in warn level.");
         return "login";
     }
     
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,16 @@
         <title>"Login Page"</title>
     </head>
     <body>
-        Welcome to login page ${name} !!
+        Welcome to login page !!
+
+        <!-- Note : By default method type is GET. To make sure the value of name and
+             password is no coming in the link (not secure) we use post method for storing
+             cred as payload  -->
+
+        <form method="post">
+            Name: <input type="text" name="Name">
+            Password: <input type="password" name="password">
+            <input type="submit"> 
+        </form>
     </body>
 </html>
