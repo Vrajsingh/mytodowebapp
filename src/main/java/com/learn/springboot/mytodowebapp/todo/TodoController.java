31493494a008239358ddcd4f64fa8182abba6d87
Vishal Raj <vishalraj@Viveks-MacBook-Pro.local>
2024-07-03 00:44:53 +0530

    Created new Add-todo api with todo.jsp

--- src/main/java/com/learn/springboot/mytodowebapp/todo/TodoController.java
+++ src/main/java/com/learn/springboot/mytodowebapp/todo/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 
@@ -24,6 +25,25 @@ public class TodoController {
 
         model.addAttribute("todos", todos);
         return "listTodos";
+    }
+
+    // handles GET, POST, ...
+    // @RequestMapping("add-todo")
+    // public String showNewTodoPage() {
+    //     return "todo";
+    // }
+
+    // for handling GET
+    @RequestMapping(value="add-todo", method = RequestMethod.GET)
+    public String showNewTodoPage() {
+        return "todo";
+    }
+
+    // for handling POST
+    @RequestMapping(value="add-todo", method = RequestMethod.POST)
+    public String addNewTodo() {
+
+        return "redirect:list-todos";
     }
     
 }
