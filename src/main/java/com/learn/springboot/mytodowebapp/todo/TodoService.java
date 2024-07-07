dc4265f2ba7b9f23c02bc7f5ec3e50d8f3428c44
Vishal Raj <vishalraj@Viveks-MacBook-Pro.local>
2024-07-07 20:49:58 +0530

    Implemented save todo page

--- src/main/java/com/learn/springboot/mytodowebapp/todo/TodoService.java
+++ src/main/java/com/learn/springboot/mytodowebapp/todo/TodoService.java
@@ -7,6 +7,8 @@ import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.Valid;
+
 @Service
 public class TodoService {
     
@@ -46,6 +48,12 @@ public class TodoService {
         Predicate<? super Todo> predicate = todo -> todo.getId() == id ;
         Todo todo = todos.stream().filter(predicate).findFirst().get();
         return todo;
+    }
+
+    public void updateTodo(@Valid Todo todo) {
+        deleteById(todo.getId());
+        todos.add(todo);
+       
     }
 
     
