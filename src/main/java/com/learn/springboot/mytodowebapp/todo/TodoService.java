ec6db4d182bfdd264e152a7cd12da1e8b175a512
Vishal Raj <vishalraj@Viveks-MacBook-Pro.local>
2024-07-07 20:40:05 +0530

    implemented show update page

--- src/main/java/com/learn/springboot/mytodowebapp/todo/TodoService.java
+++ src/main/java/com/learn/springboot/mytodowebapp/todo/TodoService.java
@@ -41,4 +41,12 @@ public class TodoService {
         Predicate<? super Todo> predicate = todo -> todo.getId() == id ;
         todos.removeIf(predicate);
     }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id ;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+    }
+
+    
 }
